2928f11e4b74016e271a0289be86f8e35c296de8
Mohammad Javad Akbari <mojav.akbari@gmail.com>
2024-06-26 16:56:22 +0330

    css light dark

--- src/main/java/org/aut/polylinked_client/control/LoginController.java
+++ src/main/java/org/aut/polylinked_client/control/LoginController.java
@@ -1,18 +1,33 @@
 package org.aut.polylinked_client.control;
 
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import org.aut.polylinked_client.PolyLinked;
+import javafx.scene.control.Label;
 
 public class LoginController {
+
+    @FXML
+    private Label messageText;
+
+    @FXML
+    private JFXTextField emailText;
+
+    @FXML
+    private JFXTextField passwordText;
+
+    @FXML
+    void forgotPasswordPressed(ActionEvent event) {
+
+    }
+
     @FXML
     void loginPressed(ActionEvent event) {
-        PolyLinked.setScene(PolyLinked.SceneLevel.HOME);
+
     }
 
     @FXML
     void signupPressed(ActionEvent event) {
-        PolyLinked.setScene(PolyLinked.SceneLevel.SIGNUP);
-    }
 
+    }
 }
